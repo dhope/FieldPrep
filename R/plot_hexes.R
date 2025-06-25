e71e0d20e26bf001ef777dfbdc6cc4888512fc10
David Hope <david.hope@ec.gc.ca>
2025-06-25 15:21:26 -0400

    Add table to plot plots

--- R/plot_hexes.R
+++ R/plot_hexes.R
@@ -7,6 +7,7 @@
 #' @param near_water_course
 #' @param water_body
 #' @param crs_to_use
+#' @param ... For future expansions. Currently only 'text_size' included.
 #'
 #' @returns
 #' @export
@@ -19,7 +20,11 @@ plot_hex <- function(plot_id,
                      sites_sf_sf,
                      near_water_course,
                      water_body,
-                     crs_to_use= 3161, show_tz=FALSE){
+                     crs_to_use= 3161, show_tz=FALSE, ...){
+  args <- list(...)
+  if(!'label_text_size' %in% names(args)) args$label_text_size <- 10
+  if(!'site_tz_size' %in% names(args)) args$site_tz_size <- 6
+  if(!'site_text_size' %in% names(args)) args$site_text_size <- 6
 
   # ex <- plots$PSU_ID[[i]]
   ARUtools:::check_cols(plot_sf_df, {{plot_col_label}})
@@ -52,12 +57,14 @@ plot_hex <- function(plot_id,
   })
 
 
-  ggplot2::ggplot() +
-    gen_geoms(wc_loc, wb_loc, PSU, loc_sites, show_tz, {{site_col_label}}) +
+
+
+  plt <- ggplot2::ggplot() +
+    gen_geoms(wc_loc, wb_loc, PSU, loc_sites, show_tz, {{site_col_label}}, args) +
     ggplot2::theme_void() +
     # ggplot2::labs(title = plot_id) +
     geom_sf_text(data = lab_loc, aes(label = label),
-                 size = 10)+
+                 size = args$label_text_size)+
     ggplot2::theme(legend.position = 'none',
           plot.title = element_text(hjust = .8),
     ) +
@@ -67,11 +74,28 @@ plot_hex <- function(plot_id,
       style =
         ggspatial::north_arrow_minimal(line_width = 2,
                                        text_col = 'white'))
+
+  if(isTRUE(args$add_table)){
+    if(rlang::is_installed('cowplot') &&
+       rlang::is_installed('gridExtra')){
+    full_site_tab <- args$aru_n[args$aru_n$PlotID==plot_id,c("SiteNumber", "ARU_ID")]
+    tab_plt <- gridExtra::tableGrob(full_site_tab,
+                                                       rows= NULL, cols = NULL,
+                                                       theme =gridExtra::ttheme_minimal())
+
+    return(cowplot::ggdraw(plt) + cowplot::draw_grob(tab_plt,
+                                                 x = .12, y = .68,
+                                                 width = 0.2, height = 0.2 * 10/6 ) )
+    } else{rlang::abort("Packages 'cowplot'  and 'gridExtra' are required to add tables")}
+
+  } else{plt}
+
+    tab_plt
 }
 
 
 
-gen_geoms <- function(wc_loc, wb_loc, PSU, loc_sites, show_tz, site_col_label){
+gen_geoms <- function(wc_loc, wb_loc, PSU, loc_sites, show_tz, site_col_label, args){
   list(
     ggplot2::geom_sf(data =wc_loc,
                      colour = 'skyblue'),
@@ -80,17 +104,17 @@ gen_geoms <- function(wc_loc, wb_loc, PSU, loc_sites, show_tz, site_col_label){
       ggplot2::geom_sf(data = PSU,fill = NA ) ,
        if(show_tz){
          geom_sf(data = loc_sites,aes(shape = tz),
-            size=6
+            size=args$site_tz_size
     )},
     if(show_tz)geom_sf_text(data = loc_sites,aes(label = {{site_col_label}}),
-                   fontface = 'bold'),
+                   fontface = 'bold',size = args$site_text_size),
     if(show_tz)  scale_shape_manual(values = c("America/Toronto"=0,"America/Winnipeg"=1)) ,
 
     if(!show_tz) ggrepel::geom_text_repel(
                        data = loc_sites,
                        aes(label = {{site_col_label}}, geometry = geometry),
                        stat = "sf_coordinates",
-                       min.segment.length = 0
+                       min.segment.length = 0, size = args$site_text_size
                      )
 
   )
